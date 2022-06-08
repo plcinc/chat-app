271a9ca1b831b14dfed1276781bf2edfa1bfc60d
Paul Mun <paulmun818@gmail.com>
2022-06-08 11:28:22 +0900

    filestructure setup

---
+++ src/views/Homepage/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from 'components/Header';
+import 'views/Homepage/Homepage.scss'
+
+export default function Homepage() {
+  return (
+    <div className="homepage">
+      <Header />
+    </div>
+  );
+}
